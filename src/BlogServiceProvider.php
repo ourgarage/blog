db787d6a16272c6756dc0db531e2321ccf6b2c04
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-09-28 16:04:38 +1100

    Created struscture

---
+++ src/BlogServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ourgarage\Blog;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        require __DIR__ . '/routes/web.php';
+
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'blog');
+
+        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'blog');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('Ourgarage\Blog\Http\Controllers\BlogController');
+
+        $this->mergeConfigFrom(__DIR__.'/config/blog.php', 'packages');
+    }
+}
