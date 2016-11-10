7234e575fa12d36e2332bf8d503c33fecfbe521f
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-11-10 18:07:29 +1100

    proccess

--- src/resources/views/site/category.blade.php
+++ src/resources/views/site/category.blade.php
@@ -7,7 +7,11 @@
 @section('content')
 
     @if(isset($category->posts))
-        
+        <ul>
+            @foreach($category->posts as $post)
+                <li><a href="{{ route('blog::users::post', ['slug' => $post->slug]) }}">{{ $post->title }}</a></li>
+            @endforeach
+        </ul>
     @else
         {{ trans('blog::blog.users.no-news') }}
     @endif
