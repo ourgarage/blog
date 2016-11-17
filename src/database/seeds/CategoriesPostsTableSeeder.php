c718db164327cb7a0521787bf164b2b356dc52a4
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-11-17 23:24:24 +1100

    Fixed seed

--- src/database/seeds/CategoriesPostsTableSeeder.php
+++ src/database/seeds/CategoriesPostsTableSeeder.php
@@ -15,7 +15,7 @@ class CategoriesPostsTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-        foreach (range(1, 10) as $index) {
+        foreach (range(1, 5) as $index) {
             $category = Category::create([
                 'status' => Category::STATUS_ACTIVE,
                 'title' => $faker->word,
@@ -25,12 +25,12 @@ class CategoriesPostsTableSeeder extends Seeder
                 'meta_title' => $faker->sentence
             ]);
 
-            foreach (range(1, 50) as $post) {
+            foreach (range(1, 10) as $post) {
                 Post::create([
                     'category_id' => $category->id,
-                    'status' => Post::STATUS_ACTIVE,
+                    'status' => $faker->biasedNumberBetween($min = Post::STATUS_DISABLED, $max = Post::STATUS_ACTIVE),
                     'title' => $faker->sentence,
-                    'slug' => $faker->word,
+                    'slug' => $faker->regexify('[a-z]{5,10}'),
                     'content' => $faker->text,
                     'meta_keywords' => $faker->sentence,
                     'meta_description' => $faker->sentence,
