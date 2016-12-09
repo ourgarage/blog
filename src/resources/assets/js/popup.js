d821e192b997ea1a7e2f98ee7816c32021721df9
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-12-09 11:56:08 +1100

    Fucking javascript

--- src/resources/assets/js/popup.js
+++ src/resources/assets/js/popup.js
@@ -1,30 +1,6 @@
-/*$(function(){
-    $("[data-confirm]").on("click", function(){
+$(function () {
+    $("[data-confirm]").on("click", function () {
         var confirm = $(this).data("confirm");
         buttonConfirmation(event, confirm);
     });
-});*/
-
-$(function () {
-    $("[data-params]").on("click", function () {
-        var params = $(this).data("params");
-
-        swal({
-            title: 'Are you sure?',
-            text: params.message,
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then(function () {
-            swal(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-            )
-        })
-
-    });
 });
-
